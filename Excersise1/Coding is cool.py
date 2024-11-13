b9d4c69d484f37b65fded9ba84ff71544ac844ca
Abiha07 <abiha0907.@gmail.com>
2024-11-13 14:48:59 +0400

    Coding is cool.py

---
+++ Excersise1/Coding is cool.py
@@ -0,0 +1,12 @@
+#put in the first word for the variable word1
+word1 = "Coding"
+
+#put in the second word for the variable word2
+word2 = "is"
+
+#put in the third word for the variable word3
+word3 = "Cool"
+
+#putting all the three variable to make a sentence 
+Sum = word1 + " " + word2 + " " + word3
+print(Sum) 
