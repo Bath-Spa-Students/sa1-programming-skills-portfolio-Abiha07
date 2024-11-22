130150feefa4da7dfdb01a937c58266de726d143
Abiha07 <abiha0907.@gmail.com>
2024-11-22 18:40:49 +0400

    loops.py

---
+++ Class Activities/Loops.py
@@ -0,0 +1,11 @@
+percentage = int (input("enter your percentage :"))
+if percentage>= 90:
+      print("Your grade is A")
+elif percentage >= 80:
+      print("Your grade is B")
+elif percentage >= 70:
+      print("Your grade is C")
+elif percentage >= 60:
+      print("Your grade is D")
+else:
+      print("Your grade is F")
