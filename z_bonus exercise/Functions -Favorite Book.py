ba8ad29e84045e4e5edf3d46de169c3ddecdb7d4
Abiha07 <abiha0907.@gmail.com>
2024-11-22 19:29:08 +0400

    functions -Favorite Book.py

--- z_bonus exercise/Functions -Favorite Book.py
+++ z_bonus exercise/Functions -Favorite Book.py
@@ -3,4 +3,4 @@ def favorite_book(title):
     print(f"my top favorite book is {title}.")
 
 # processing the function with a book title
-favorite_book("Kuliyat-I-Iqbal")
+favorite_book("Kuliyat-I-Iqbal") 
